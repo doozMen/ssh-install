35979cc251ceec29a414a3ea8805e57b6166364e
Stijn Willems <stijnwillems@cowboy.com>
2021-06-23 10:47:23 +0200

    fix not logging on ci

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ssh-install",
+    platforms: [.macOS(.v10_15)],
     products: [
         // executable to run directly, it used lib `SshInstall`
         .executable(name: "ssh-install", targets: ["ssh-install"]),
@@ -13,6 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "ProcessPretty", url:"https://github.com/dooZdev/ProcessPretty.git", .upToNextMajor(from: "0.0.4")),
+        .package(url: "https://github.com/dooZdev/UnifiedLogging.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMajor(from: "0.2.2")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "0.3.0")),
     ],
@@ -21,6 +23,7 @@ let package = Package(
         .target(
             name: "SSHInstall",
             dependencies: [
+                .product(name: "UnifiedLogging", package: "UnifiedLogging"),
                 .product(name: "ProcessPretty", package: "ProcessPretty"),
                 .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
--- Sources/SSHInstall/SSHInstall.swift
+++ Sources/SSHInstall/SSHInstall.swift
@@ -2,6 +2,7 @@ import ProcessPretty
 import Foundation
 import TSCBasic
 import ArgumentParser
+import UnifiedLogging
 
 extension AbsolutePath: ExpressibleByArgument {
     public init?(argument: String) {
@@ -26,10 +27,12 @@ public struct SSHInstallCommand: ParsableCommand {
     public init() {}
     
     public func run() throws {
-        print("starting ssh-install ...")
         try runWith(privateKey: privateKey, publicKey: publicKey, config: config)
-        print("✅ ssh-install")
     }
+}
+
+private func log(text: String, color: TerminalController.Color, bold: Bool) {
+    logger.trace("SSHInstallCommand",metadata: ["output": .string(text)])
 }
 
 private func runWith(privateKey: AbsolutePath, publicKey: AbsolutePath, config: AbsolutePath) throws {
@@ -44,7 +47,11 @@ private func runWith(privateKey: AbsolutePath, publicKey: AbsolutePath, config: 
     try localFileSystem.move(from: publicKey, to: sshPath.appending(.init(publicKey.basename)))
     try localFileSystem.move(from: config, to: sshPath.appending(.init(config.basename)))
     
-    let keyscan = try ProcessPretty(executable: "ssh-keyscan", arguments: ["-H", "github.com,140.82.121.4"])
+    let keyscan = try ProcessPretty(
+        executable: "ssh-keyscan",
+        arguments: ["-H", "github.com,140.82.121.4"],
+        output: log
+    )
     
     let output = try keyscan.run(in: #function, at: #filePath)
     
@@ -57,6 +64,10 @@ private func runWith(privateKey: AbsolutePath, publicKey: AbsolutePath, config: 
     /*
      Chmod 600 (chmod a+rwx,u-x,g-rwx,o-rwx) sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can't read, can't write and can't execute. (O)thers can't read, can't write and can't execute.
      */
-    let chmodUserReadWriteOnly = try ProcessPretty(executable: "chmod", arguments: ["600", "\(localFileSystem.homeDirectory.pathString)/.ssh/\(privateKey.basename)"])
+    let chmodUserReadWriteOnly = try ProcessPretty(
+        executable: "chmod",
+        arguments: ["600", "\(localFileSystem.homeDirectory.pathString)/.ssh/\(privateKey.basename)"],
+        output: log
+    )
     try chmodUserReadWriteOnly.run(in: #function, at: #filePath)
 }
